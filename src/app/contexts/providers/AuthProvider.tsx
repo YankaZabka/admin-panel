2d53a0cc3c955f0670e917edc598a02498c27a4c
YankaZabka <yanka.zanka@gmail.com>
2022-04-21 11:25:41 +0300

    feat: add auth context and provider

---
+++ src/app/contexts/providers/AuthProvider.tsx
@@ -0,0 +1,30 @@
+/* eslint react-hooks/rules-of-hooks: 0 */
+import React, { useEffect, useState } from "react";
+import AuthContext from "../auth";
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(
+    !!localStorage.getItem("fake-token")
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("fake-token")) {
+      setLoggedIn(true);
+    }
+  }, []);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem("fake-token");
+    setLoggedIn(false);
+  };
+
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
