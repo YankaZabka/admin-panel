6f47a892e91ec5b7667d90b9d44d6376488eb112
YankaZabka <yanka.zanka@gmail.com>
2022-04-21 18:43:04 +0300

    feat: env.generated.d.ts did updates

--- src/env.generated.d.ts
+++ src/env.generated.d.ts
@@ -4,6 +4,8 @@ declare module "*/operations.gql" {
   import { DocumentNode } from "graphql";
   const defaultDocument: DocumentNode;
   export const getUsers: DocumentNode;
+  export const getAlbums: DocumentNode;
+  export const getUsersUsernames: DocumentNode;
 
   export default defaultDocument;
 }
