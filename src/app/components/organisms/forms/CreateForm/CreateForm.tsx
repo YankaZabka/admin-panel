27129f5a04422ede0311d7ad8e0d26d94833931d
YankaZabka <yanka.zanka@gmail.com>
2022-04-25 16:38:47 +0300

    feat: add mutation 'createAlbum' and query 'getUsers' to CreateForm

--- src/app/components/organisms/forms/CreateForm/CreateForm.tsx
+++ src/app/components/organisms/forms/CreateForm/CreateForm.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Button, Form, Input, Select } from "antd";
+import { useMutation, useQuery } from "@apollo/client";
+import { Button, Col, Form, Input, Row, Select, Spin } from "antd";
+import { useNavigate } from "react-router-dom";
+import { notifySuccess } from "../../../../../notify";
+import { operations as albumsOperations } from "../../AlbumsTable/duck";
+import { operations, Types } from "./duck";
 
 const { Option } = Select;
 
@@ -13,8 +18,44 @@ const tailLayout = {
 
 const CreateForm: React.FC = () => {
   const [form] = Form.useForm();
+  const navigate = useNavigate();
 
-  const onFinish = () => {};
+  const [createAlbum, { loading: mutationLoading }] = useMutation<
+    Types.CreateAlbumMutation,
+    Types.CreateAlbumMutationVariables
+  >(operations.createAlbum, {
+    onCompleted(data) {
+      navigate(-1);
+      notifySuccess(`Album '${data.createAlbum?.title}' was created!`);
+    },
+    refetchQueries: [{ query: albumsOperations.getAlbums }],
+  });
+
+  const { data: queryData, loading: queryLoading } = useQuery<
+    Types.GetUsersQuery,
+    Types.GetUsersQueryVariables
+  >(operations.getUsers);
+
+  if (!queryData || queryLoading || mutationLoading) {
+    return (
+      <Row justify="center">
+        <Col>
+          <Spin size="large" />
+        </Col>
+      </Row>
+    );
+  }
+
+  const onFinish = (values: any) => {
+    createAlbum({
+      variables: {
+        input: {
+          title: values.title,
+          userId: values.user,
+        },
+      },
+    });
+  };
 
   return (
     <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
@@ -36,16 +77,27 @@ const CreateForm: React.FC = () => {
         rules={[{ required: true }, { type: "string" }]}
       >
         <Select placeholder="Select a user" allowClear>
-          <Option value="male">male</Option>
-          <Option value="female">female</Option>
-          <Option value="other">other</Option>
+          {queryData.users?.data?.map((item) => {
+            return (
+              <Option key={item?.id} value={item?.id}>
+                {item?.username}
+              </Option>
+            );
+          })}
         </Select>
       </Form.Item>
       <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit" style={{ marginRight: "8px" }}>
+        <Button
+          type="primary"
+          htmlType="submit"
+          style={{ marginRight: "8px" }}
+          onClick={() => {}}
+        >
           Submit
         </Button>
-        <Button htmlType="button">Cancel</Button>
+        <Button htmlType="button" onClick={() => navigate(-1)}>
+          Cancel
+        </Button>
       </Form.Item>
     </Form>
   );
--- src/env.generated.d.ts
+++ src/env.generated.d.ts
@@ -3,11 +3,12 @@
 declare module "*/operations.gql" {
   import { DocumentNode } from "graphql";
   const defaultDocument: DocumentNode;
-  export const getUsers: DocumentNode;
   export const getAlbumInfo: DocumentNode;
   export const getAlbums: DocumentNode;
   export const getPhotoInfo: DocumentNode;
   export const getAlbumPhotoInfo: DocumentNode;
+  export const getUsers: DocumentNode;
+  export const createAlbum: DocumentNode;
   export const getUsersUsernames: DocumentNode;
 
   export default defaultDocument;
