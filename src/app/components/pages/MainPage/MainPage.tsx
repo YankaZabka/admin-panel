ae2e0287f9b00f2f37e597e09bffbe09d82bbc7c
YankaZabka <yanka.zanka@gmail.com>
2022-05-19 11:53:22 +0300

    fix: delete AnimationContainer

--- src/app/components/pages/MainPage/MainPage.tsx
+++ src/app/components/pages/MainPage/MainPage.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import PrivateRoute from "../../molecules/PrivateRoute";
 import AdminPage from "../../organisms/AdminPage";
-import AnimationContainer from "../../organisms/AnimationContainer/AnimationContainer";
 
 const MainPage: React.FC = () => {
   return (
     <PrivateRoute>
-      <AnimationContainer>
-        <AdminPage />
-      </AnimationContainer>
+      <AdminPage />
     </PrivateRoute>
   );
 };
