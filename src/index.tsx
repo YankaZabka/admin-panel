dd595caf1048fbdd1a29b0a13425ee4f77eda439
YankaZabka <yanka.zanka@gmail.com>
2022-05-19 12:25:22 +0300

    feat: add ChakraProvider to main index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom";
 import App from "./app";
+import theme from "./app/theme";
+import "./app/theme/fonts.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
