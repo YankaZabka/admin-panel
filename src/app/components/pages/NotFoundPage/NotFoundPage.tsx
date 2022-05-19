89306e9b3b9c45ba8294dcf17b794d71a41db41f
YankaZabka <yanka.zanka@gmail.com>
2022-05-19 11:51:56 +0300

    feat: rebuild NotFoundPage with Chakra

--- src/app/components/pages/NotFoundPage/NotFoundPage.tsx
+++ src/app/components/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,19 +1,37 @@
 import React from "react";
-import { Button, Result } from "antd";
+import {
+  Flex,
+  Text,
+  useColorModeValue,
+  VStack,
+  Button,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const NotFoundPage: React.FC = () => {
+  const bgColor = useColorModeValue("gray.100", "gray.700");
+
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={
-        <Button type="primary">
-          <Link to="/">Back Home</Link>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <VStack
+        direction="column"
+        bg={bgColor}
+        p={12}
+        rounded={6}
+        maxW="600px"
+        spacing={8}
+      >
+        <Text fontSize="4xl" fontWeight="bold" align="center">
+          404!
+        </Text>
+        <Text fontSize="xl" fontWeight="bold" as="em" align="center">
+          Sorry, the page you visited does not exist.
+        </Text>
+        <Button colorScheme="teal" variant="solid">
+          <Link to="/">Back home</Link>
         </Button>
-      }
-    />
+      </VStack>
+    </Flex>
   );
 };
 
