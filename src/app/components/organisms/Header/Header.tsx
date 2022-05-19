3c81085a97acca6f91699551c1cf392bb7fae67d
YankaZabka <yanka.zanka@gmail.com>
2022-05-19 12:17:45 +0300

    feat: rebuild Header with Chakra

--- src/app/components/organisms/Header/Header.tsx
+++ src/app/components/organisms/Header/Header.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { CalendarIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import {
-  HomeOutlined,
-  PictureOutlined,
-  CalendarOutlined,
-  LogoutOutlined,
-} from "@ant-design/icons/lib";
-import { PageHeader } from "antd";
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  Heading,
+  Spacer,
+  useColorModeValue,
+  Icon,
+  useColorMode,
+} from "@chakra-ui/react";
+import { BiPhotoAlbum, BiLogOut } from "react-icons/bi";
+import { RiHome3Fill } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
-import HeaderButton from "../../atoms/HeaderButton";
 
 const Header: React.FC = () => {
   const auth: any = useAuth();
   const navigate = useNavigate();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bgColor = useColorModeValue("gray.100", "gray.700");
 
   const onLogout = () => {
     auth.logOut();
@@ -31,40 +39,52 @@ const Header: React.FC = () => {
   };
 
   return (
-    <PageHeader
-      className="site-page-header"
-      title="Admin-panel"
-      extra={
-        auth.loggedIn
-          ? [
-              <HeaderButton
-                title="Home"
-                key="1"
-                onClick={onHome}
-                icon={<HomeOutlined />}
-              />,
-              <HeaderButton
-                title="Albums"
-                key="2"
-                onClick={onAlbums}
-                icon={<PictureOutlined />}
-              />,
-              <HeaderButton
-                title="DateRange"
-                key="3"
-                onClick={onDateRange}
-                icon={<CalendarOutlined />}
-              />,
-              <HeaderButton
-                title="Log Out"
-                key="4"
-                onClick={onLogout}
-                icon={<LogoutOutlined />}
-              />,
-            ]
-          : null
-      }
-    />
+    <Flex
+      minWidth="max-content"
+      alignItems="center"
+      gap="2"
+      p={4}
+      w="100%"
+      bg={bgColor}
+    >
+      <Box p="2">
+        <Heading size="md">Admin Panel</Heading>
+      </Box>
+      <Spacer />
+      <ButtonGroup gap="2">
+        <Button colorScheme="teal" spacing={0} onClick={toggleColorMode}>
+          <Icon as={colorMode === "light" ? MoonIcon : SunIcon} w={5} h={5} />
+        </Button>
+        <Button
+          leftIcon={<Icon as={RiHome3Fill} w={5} h={5} />}
+          colorScheme="teal"
+          onClick={onHome}
+        >
+          Home
+        </Button>
+        <Button
+          leftIcon={<Icon as={BiPhotoAlbum} w={5} h={5} />}
+          colorScheme="teal"
+          onClick={onAlbums}
+        >
+          Albums
+        </Button>
+        <Button
+          leftIcon={<CalendarIcon />}
+          colorScheme="teal"
+          onClick={onDateRange}
+        >
+          DateRange
+        </Button>
+        <Button
+          leftIcon={<Icon as={BiLogOut} w={5} h={5} />}
+          colorScheme="teal"
+          onClick={onLogout}
+        >
+          Log out
+        </Button>
+      </ButtonGroup>
+    </Flex>
   );
 };
 
